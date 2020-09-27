e676cf652d8e23e4a4bb526aa67ad9a528ef0818
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-09-28 00:29:01 +0300

    feat: add "Repository", update gitignore

---
+++ src/Life-Balance.BLL/Repository/Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Life_Balance.BLL.Repository
+{
+    class Repository
+    {
+    }
+}
---
+++ src/Life-Balance.Commin/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Life_Balance.Common.Interfaces
+{
+    interface IRepository
+    {
+    }
+}
