aee6d3c70d95757a2481fab8c9b4ec0f34d66afe
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-22 22:09:16 +0300

    feat: add view model and DiaryEntry view

--- src/Life-Balance.WebApp/Controllers/DiaryController.cs
+++ src/Life-Balance.WebApp/Controllers/DiaryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Life_Balance.WebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Life_Balance.WebApp.Controllers
 {
@@ -7,6 +9,25 @@ namespace Life_Balance.WebApp.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        
+        /// <summary>
+        /// For new diary entry
+        /// </summary>
+        /// <returns>View page</returns>
+        public IActionResult DiaryEntry()
+        {
+            return View();
+        }
+        
+        /// <summary>
+        /// For new diary entry
+        /// </summary>
+        /// <param name="model">Diary ViewModel</param>
+        /// <returns></returns>
+        public async Task<IActionResult> DiaryEntry(DiaryEntryViewModel model)
+        {
+            return View(model);
         }
     }
 }
---
+++ src/Life-Balance.WebApp/ViewModels/DiaryEntryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Life_Balance.WebApp.ViewModels
+{
+    public class DiaryEntryViewModel
+    {
+        /// <summary>
+        /// Title entry.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Entry in diary.
+        /// </summary>
+        public string Entry { get; set; }
+
+        /// <summary>
+        /// Date when the log entry was made.
+        /// </summary>
+        public DateTime Date { get; set; }
+    }
+}
