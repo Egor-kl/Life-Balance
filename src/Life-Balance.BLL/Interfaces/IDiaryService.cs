363ecb1db007805311e50899703f69aee0e0ea6a
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-23 06:49:42 +0300

    feat: add diary service interface

---
+++ src/Life-Balance.BLL/Interfaces/IDiaryService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Life_Balance.BLL.Interfaces
+{
+    public interface IDiaryService
+    {
+        /// <summary>
+        /// Get entry by date.
+        /// </summary>
+        /// <param name="dateTime">Date for search.</param>
+        /// <returns></returns>
+        public Task GetEntryByDate(DateTime dateTime);
+
+        /// <summary>
+        /// Create new entry.
+        /// </summary>
+        /// <param name="title">Title entry.</param>
+        /// <param name="entries">Notes</param>
+        /// <param name="dateTime">Date</param>
+        /// <returns></returns>
+        public Task CreateNewEntry(string title, string entries, DateTime dateTime);
+
+        /// <summary>
+        /// Delete by id.
+        /// </summary>
+        /// <param name="entryId">Id entry.</param>
+        /// <returns></returns>
+        public Task DeleteEntry(string entryId);
+
+        /// <summary>
+        /// Update entry.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="entries"></param>
+        /// <returns></returns>
+        public Task UpdateEntry(string title, string entries);
+    }
+}
---
+++ src/Life-Balance.BLL/Services/DiaryService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Life_Balance.BLL.Interfaces;
+
+namespace Life_Balance.BLL.Services
+{
+    public class DiaryService : IDiaryService
+    {
+        public Task GetEntryByDate(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateNewEntry(string title, string entries, DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEntry(string entryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEntry(string title, string entries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
