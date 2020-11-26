5dd33c0b0f2184e08cb21e0e22ed3bea759f67b4
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-26 21:09:28 +0300

    feat: add new method in diary controller (DiaryEntry)

--- src/Life-Balance.WebApp/Controllers/DiaryController.cs
+++ src/Life-Balance.WebApp/Controllers/DiaryController.cs
@@ -1,4 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Life_Balance.BLL.Interfaces;
+using Life_Balance.BLL.Models;
+using Life_Balance.BLL.ModelsDTO;
+using Life_Balance.Common.Interfaces;
+using Life_Balance.DAL.Models;
 using Life_Balance.WebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +13,19 @@ namespace Life_Balance.WebApp.Controllers
 {
     public class DiaryController : Controller
     {
+        private readonly IDiaryService _diaryService;
+        private readonly IRepository<Diary> _diaryRepository;
+        private readonly IMapper _mapper;
+        private readonly IEmailService _email;
+
+        public DiaryController(IDiaryService diaryService,IRepository<Diary> diaryRepository, IMapper mapper, IEmailService email)
+        {
+            _diaryRepository = diaryRepository ?? throw new ArgumentNullException(nameof(diaryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _diaryService = diaryService ?? throw new ArgumentNullException(nameof(diaryService));
+            _email = email ?? throw new ArgumentNullException(nameof(email));
+        }
+        
         public IActionResult Index()
         {
             return View();
@@ -25,8 +45,16 @@ namespace Life_Balance.WebApp.Controllers
         /// </summary>
         /// <param name="model">Diary ViewModel</param>
         /// <returns></returns>
+        [HttpPost]
         public async Task<IActionResult> DiaryEntry(DiaryEntryViewModel model)
         {
+            if(ModelState.IsValid)
+            {
+                var diaryDto = new DiaryDTO { Title = model.Title, Date = model.Date, Entries = model.Entry};
+                var result = _mapper.Map<Diary>(diaryDto);
+                await _diaryService.CreateNewEntry(result, model.Title, model.Entry, DateTime.Now);
+            }
+
             return View(model);
         }
     }
