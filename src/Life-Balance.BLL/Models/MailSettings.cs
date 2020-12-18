c37e92757b59440fbf5467c04606e3b297f27a35
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-12-18 15:44:18 +0300

    feat: add mailsettings

---
+++ src/Life-Balance.BLL/Models/MailSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Life_Balance.BLL.Models
+{
+    public class MailSettings
+    {
+        /// <summary>
+        /// Server.
+        /// </summary>
+        public string Server { get; set; }
+
+        /// <summary>
+        /// Port.
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// Email.
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
