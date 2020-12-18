4227139d33ed4f5fc4c60cc3a704be7bb24fba42
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-12-18 15:44:18 +0300

    feat: add mailsettings

--- src/Life-Balance.BLL/Services/EmailService.cs
+++ src/Life-Balance.BLL/Services/EmailService.cs
@@ -1,7 +1,11 @@
-﻿using Life_Balance.Common.Interfaces;
+﻿using System;
+using Life_Balance.Common.Interfaces;
 using MailKit.Net.Smtp;
 using MimeKit;
 using System.Threading.Tasks;
+using Life_Balance.BLL.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Life_Balance.BLL.Services
 {
@@ -10,11 +14,18 @@ namespace Life_Balance.BLL.Services
     /// </summary>
     public class EmailService : IEmailService
     {
+        private readonly MailSettings _mailConfig;
+
+        public EmailService()
+        {
+            _mailConfig = MailKitConfiguration();
+        }
+        
         public async Task SendEmailAsync(string email, string subject, string message)
         {
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("Life Balance", ""));
+            emailMessage.From.Add(new MailboxAddress("Life Balance", _mailConfig.EmailAddress));
             emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
@@ -24,12 +35,24 @@ namespace Life_Balance.BLL.Services
 
             using (var client = new SmtpClient())
             {
-                await client.ConnectAsync("smtp.gmail.com", 587, false);
-                await client.AuthenticateAsync("@gmail.com","");
+                await client.ConnectAsync(_mailConfig.Server, _mailConfig.Port, false);
+                await client.AuthenticateAsync(_mailConfig.EmailAddress,_mailConfig.Password);
                 await client.SendAsync(emailMessage);
 
                 await client.DisconnectAsync(true);
             }
+        }
+        
+        private MailSettings MailKitConfiguration()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("mailsettings.json")
+                .Build();
+
+            var mailSettingSection = configuration.GetSection("MailSettings");
+            var mailSettings = mailSettingSection.Get<MailSettings>();
+
+            return mailSettings;
         }
     }
 }
