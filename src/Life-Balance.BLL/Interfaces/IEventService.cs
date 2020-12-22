9d90c56c6cb697c2f45de335c6beee517c8c4f17
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-12-22 11:41:25 +0300

    feat: add event interface and event dto

---
+++ src/Life-Balance.BLL/Interfaces/IEventService.cs
@@ -0,0 +1,16 @@
+﻿using Life_Balance.BLL.ModelsDTO;
+using System.Threading.Tasks;
+
+namespace Life_Balance.BLL.Interfaces
+{
+    public interface IEventService
+    {
+        /// <summary>
+        /// Create new event
+        /// </summary>
+        /// <param name="events">Event DTO</param>
+        /// <param name="userId">User Id.</param>
+        /// <returns></returns>
+        public Task Create(EventDTO events, string userId);
+    }
+}
---
+++ src/Life-Balance.BLL/ModelsDTO/EventDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Life_Balance.BLL.ModelsDTO
+{
+    public class EventDTO
+    {
+        /// <summary>
+        /// ID Key.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title of meeting.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Write some notes.
+        /// </summary>
+        public string Note { get; set; }
+
+        /// <summary>
+        /// Meetings start date.
+        /// </summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// Meetings end date.
+        /// </summary>
+        public DateTime End { get; set; }
+
+
+        public string UserId { get; set; }
+    }
+}
