ad3ddee7b83b579f7a50fd2a5127342182a8caea
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-01-16 10:42:01 +0300

    feat: add validation

---
+++ src/Life-Balance.BLL/Extensions/Validator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Life_Balance.DAL.Models;
+
+namespace Life_Balance.BLL.Extensions
+{
+    public class Validator : AbstractValidator<User>
+    {
+        public Validator()
+        {
+            RuleFor(user => user.Email).EmailAddress();
+            RuleFor(user => user.PasswordHash)
+                .Must(x => x.Length >= 6)
+                .WithMessage("Password need > 6");
+        }
+    }
+}
